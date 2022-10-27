d42cbfc94c879a88e90a9c53b7bc1ec38daf4a5a
alex <a.jurcenko@inproduct.de>
2022-10-27 21:46:13 +0200

    Created rgb channels and ir_input

---
+++ main.py
@@ -0,0 +1,14 @@
+from machine import Pin
+
+_r_channel = Pin(0, Pin.OUT)
+_g_channel = Pin(1, Pin.OUT)
+_b_channel = Pin(2, Pin.OUT)
+_ir_in = Pin(3, Pin.IN)
+
+
+def setup():
+    pass
+
+
+if __name__ == '__main__':
+    pass
